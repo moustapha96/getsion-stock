099c663c3c2f7c75c970d2ab96bf63ef1900a882
Al hussein Khouma <alhusseinkhouma0@gmail.com>
2026-03-30 12:09:44 +0000

    Initial commit

---
+++ assets/date-format-qL-o8H-f.js
@@ -0,0 +1,1 @@
+const e=t=>new Date(t).toLocaleDateString("fr-FR",{day:"2-digit",month:"long",year:"numeric"});export{e as f};
---
+++ assets/om-image-BpNskbO7.js
@@ -0,0 +1,1 @@
+const a="/assets/wave-image-DnO_I3CT.png",s="/assets/om-image-CBEzpwAR.png";export{s as o,a as w};
